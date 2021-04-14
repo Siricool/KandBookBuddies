46e141f8ebb8a79ea16aef650a568408a3c8b2e1
Siricool <siri.nordlander@hotmail.com>
2021-04-14 11:25:14 +0200

    Screen fix #1

--- Screens/StartPageScreen.js
+++ Screens/StartPageScreen.js
@@ -2,7 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+//import React from 'react'
+//import { Text, View } from 'react-native'
+
+export default function StartPageScreen(props) {
+    return (
+        <View>
+            <Text>Start Page Screen</Text>
+        </View>
+    )
+}
+/*export default function App() {
   return (
     <View style={styles.container}>
       //<Text></Text>
@@ -19,3 +29,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+*/
