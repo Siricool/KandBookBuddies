5c2b72ae8612fa965a4adca1f43ee80d574d6529
jullemusten <hellstrom.julia@hotmail.com>
2021-04-19 15:36:59 +0200

    redux fix inlog funkar

--- src/screens/RegistrationScreen.js
+++ src/screens/RegistrationScreen.js
@@ -7,10 +7,149 @@ import { Image, TextInput, TouchableOpacity } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import styles from './RegStyle';
 
-import { firebase } from '../firebase/config'
+import firebase from 'firebase/app'
+/*import { firebase } from '../firebase/config'*/
 
+import { connect } from 'react-redux';
+import { setPersonData, watchPersonData } from '../../redux/App-redux';
 
-export default function RegistrationScreen({navigation}) {
+import { NavigationActions } from 'react-navigation';
+
+/*const setStateFunction= () => {
+    const [fullName, setFullName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+
+};*/
+
+export default class RegistrationScreen extends React.Component {
+    /*setStateFunction;*/
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            fullName: "",
+            email: "",
+            password: "",
+            confirmPassword: ""
+        }
+        
+    }
+
+    onFooterLinkPress = () => {
+        this.props.navigation.navigate('LogInScreen')
+    }
+
+    /*onFooterLinkPress = () => {
+        this.props.navigation.navigate('LogInScreen')
+        /*navigation.navigate('LogInScreen')*/
+        /*var navActions = NavigationActions.reset({
+            index: 0,
+            actions: [NavigationActions.navigate({routeName: "LogInScreen"})]
+        });
+        this.props.navigation.dispatch(navActions);*/
+    /*}*/
+
+    //const onRegisterPress = () => {
+    //}
+
+    onRegisterPress = () => {
+        if (this.state.password !== this.state.confirmPassword) {
+            alert("Passwords don't match.")
+            return
+        }
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(this.state.email, this.state.password)
+            .then(() => { }, (error) => { alert(error.message); });
+
+               /* const uid = response.user.uid
+                const data = {
+                    id: uid,
+                    email,
+                    fullName,
+                };
+                const usersRef = firebase.firestore().collection('users')
+                usersRef
+                    .doc(uid)
+                    .set(data)
+                    .then(() => {
+                        navigation.navigate('StartPageScreen')
+                    })
+                    .catch((error) => {
+                        alert(error)
+                    });
+            })
+            .catch((error) => {
+                alert(error)
+        });*/
+        this.props.navigation.navigate('StartPageScreen')
+    }
+
+    render() {
+    return (
+        <View style={styles.container}>
+            <KeyboardAwareScrollView
+                style={{ flex: 1, width: '100%' }}
+                keyboardShouldPersistTaps="always">
+                <Image
+                    style={styles.logo}
+                    source={require('../../assets/BBicon.png')}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='Full Name'
+                    placeholderTextColor="#aaaaaa"
+                    onChangeText={(text) => {this.setState({fullName: text})} }
+                    value={this.state.fullName}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='E-mail'
+                    placeholderTextColor="#aaaaaa"
+                    onChangeText={(text) => {this.setState({email: text})} }
+                    value={this.state.email}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholderTextColor="#aaaaaa"
+                    secureTextEntry
+                    placeholder='Password'
+                    onChangeText={(text) => {this.setState({password: text})} }
+                    value={this.state.password}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholderTextColor="#aaaaaa"
+                    secureTextEntry
+                    placeholder='Confirm Password'
+                    onChangeText={(text) => {this.setState({confirmPassword: text})} }
+                    value={this.state.confirmPassword}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => this.onRegisterPress()}>
+                    <Text style={styles.buttonTitle}>Create account</Text>
+                </TouchableOpacity>
+                <View style={styles.footerView}>
+                    <Text style={styles.footerText}>Already got an account? <Text onPress={this.onFooterLinkPress} style={styles.footerLink}>Log in</Text></Text>
+                </View>
+            </KeyboardAwareScrollView>
+        </View>
+    )
+    }
+}
+
+/*export default function RegistrationScreen({navigation}) {
 
     const [fullName, setFullName] = useState('')
     const [email, setEmail] = useState('')
@@ -18,7 +157,7 @@ export default function RegistrationScreen({navigation}) {
     const [confirmPassword, setConfirmPassword] = useState('')
 
     const onFooterLinkPress = () => {
-        navigation.navigate('Login')
+        navigation.navigate('LogInScreen')
     }
 
     //const onRegisterPress = () => {
@@ -44,7 +183,7 @@ export default function RegistrationScreen({navigation}) {
                     .doc(uid)
                     .set(data)
                     .then(() => {
-                        navigation.navigate('StartPageScreen', {user: data})
+                        navigation.navigate('StartPageScreen')
                     })
                     .catch((error) => {
                         alert(error)
@@ -113,7 +252,10 @@ export default function RegistrationScreen({navigation}) {
             </KeyboardAwareScrollView>
         </View>
     )
-}
+}*/
+
+/*export default connect(mapStateToProps, mapDispatchToProps)(RegistrationScreen);*/
+
 /*export default function App() {
   return (
     <View style={styles.container}>
