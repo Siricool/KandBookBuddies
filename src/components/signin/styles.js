9d1b4d899f5f252186177962f3d71d353dd54aa1
jullemusten <hellstrom.julia@hotmail.com>
2021-04-26 09:15:55 +0200

    design login page

--- src/components/signin/styles.js
+++ src/components/signin/styles.js
@@ -1,4 +1,86 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    container: {
+        /*flex: 1,*/
+        /*alignItems: 'center',*/
+        backgroundColor: '#fde3b7'
+    },
+    title: {
+
+    },
+    logo: {
+        flex: 1,
+        height: 120,
+        width: 120,
+        alignSelf: "center",
+        margin: 30
+    },
+    text: {
+        fontSize: 30,
+        fontWeight: "bold",
+        marginTop: 10,
+        marginLeft: 30,
+        marginRight: 30,
+        fontFamily: 'Helvetica' 
+    },
+    smallText: {
+        fontSize: 15,
+        color: 'grey',
+        marginTop: 5,
+        marginBottom: 10,
+        marginLeft: 30,
+        marginRight: 30,
+        fontFamily: 'Helvetica' 
+    },
+    input: {
+        height: 48,
+        borderRadius: 5,
+        overflow: 'hidden',
+        backgroundColor: 'white',
+        marginTop: 10,
+        marginBottom: 10,
+        marginLeft: 30,
+        marginRight: 30,
+        paddingLeft: 16,
+        fontFamily: 'Helvetica' 
+    },
+    button: {
+        backgroundColor: '#f9ad30',
+        marginLeft: 30,
+        marginRight: 30,
+        marginTop: 20,
+        height: 48,
+        borderRadius: 5,
+        alignItems: "center",
+        justifyContent: 'center',
+        fontFamily: 'Helvetica' 
+    },
+    buttonTitle: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: "bold",
+        fontFamily: 'Helvetica' 
+    },
+    footerView: {
+        flex: 1,
+        alignItems: "center",
+        marginTop: 20,
+    },
+    footerText: {
+        fontSize: 16,
+        color: '#2e2e2d',
+        fontFamily: 'Helvetica'
+    },
+    footerLink: {
+        color: "#f9ad30",
+        fontWeight: "bold",
+        fontSize: 16,
+        fontFamily: 'Helvetica' 
+    }
+})
+
+/*import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
     container: {
@@ -56,4 +138,4 @@ export default StyleSheet.create({
         fontWeight: "bold",
         fontSize: 16
     }
-})
+})*/
