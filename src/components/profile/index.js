7d9124b00384b9cc8eaaddd0446d6f9ad72ca078
jullemusten <hellstrom.julia@hotmail.com>
2021-05-05 10:39:55 +0200

    new toolbar images and some design fix

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import styles from '../styles';
-import {selectCartItems} from '../../redux/Cart/cart.selectors';
+import { selectCartItems } from '../../redux/Cart/cart.selectors';
 import { createStructuredSelector } from 'reselect';
 import { ScrollView } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Image, TouchableHighlight, TouchableOpacity, View, Text } from 'react-native';
+import { Icon } from 'react-native-elements'
 
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
@@ -18,94 +19,127 @@ const mapStateCart = createStructuredSelector({
     cartItems: selectCartItems
 });
 
-const MyProfile = ({navigation}) => {
+const MyProfile = ({ navigation }) => {
     const { currentUser } = useSelector(mapState);
     const { updatedUser } = useSelector(mapState);
     const { cartItems } = useSelector(mapStateCart);
 
     const renderElement = () => {
         if (updatedUser != null) {
-          return <Text> {updatedUser.groupID} </Text>;
+            return <Text> {updatedUser.groupID} </Text>;
         }
         else {
-          return <Text> {currentUser.groupID} </Text>;
+            return <Text> {currentUser.groupID} </Text>;
         }
-      };
+    };
 
     return (
         <View style={styles.container}>
             <KeyboardAwareScrollView style={{ width: '100%', height: '90%' }}>
                 <Text style={styles.text}><Text style={styles.capital}>{currentUser.displayName}</Text>'s Profile</Text>
-                <Text style={styles.middleTextOrange}></Text> 
+                <Text style={styles.middleTextOrange}></Text>
                 <Text style={styles.middleText}>My Stats</Text>
                 <Text style={styles.middleText}>My Book Clubs</Text>
                 <View style={styles.left}>
-                <TouchableOpacity style={styles.coolButton}
-                    onPress={() => navigation.navigate('BCView')}> 
-                    <Text style={styles.buttonText}>
-                        {renderElement()}
-                    </Text>
-                </TouchableOpacity>
+                    <TouchableOpacity style={styles.coolButton}
+                        onPress={() => navigation.navigate('BCView')}>
+                        <Text style={styles.buttonText}>
+                            {renderElement()}
+                        </Text>
+                    </TouchableOpacity>
                 </View>
 
                 <Text style={styles.middleText}>My Read Books</Text>
-                
-                <SafeAreaView style={styles.whiteBigSquare}>   
-                 <Text style={styles.middleText}>My Wish List </Text>
+
+                <SafeAreaView style={styles.whiteBigSquare}>
+                    <Text style={styles.middleText}>My Wish List </Text>
                     <ScrollView vertical={true} style={styles.rowBooks}>
                         {cartItems.map((cartItem, index) => {
                             const bookurl = { uri: cartItem.picture }
                             return (
-                                <View key = {cartItem.title} style={styles.orangeSquare}>
-                                    <Text  style={styles.smallTextOrange}> {cartItem.title} </Text>
-                                    <Image 
-                                    style={styles.bookImageSmall}
-                                    source={bookurl} /> 
+                                <View key={cartItem.title} style={styles.orangeSquare}>
+                                    <Text style={styles.smallTextOrange}> {cartItem.title} </Text>
+                                    <Image
+                                        style={styles.bookImageSmall}
+                                        source={bookurl} />
                                     <Text style={styles.smallerText}> {cartItem.author} </Text>
-                                </View>    
+                                </View>
                             )
                         })}
                     </ScrollView>
                 </SafeAreaView>
-               
 
-                </KeyboardAwareScrollView>
-                <View style={styles.row}>
-                    <TouchableHighlight onPress={() => navigation.navigate('MyProfile')}>
-                        <Image
-                            style={styles.menuToolbar}
-                            source={require('../../../assets/Profile_picture.png')}
-                        />
-                    </TouchableHighlight>
-                    <TouchableHighlight onPress={() => navigation.navigate('BCOverview')}>
-                        <Image
-                            style={styles.menuToolbar}
-                            source={require('../../../assets/BookClubs_picture.png')}
-                        />
-                    </TouchableHighlight>
-                    <TouchableHighlight onPress={() => navigation.navigate('StartPage')}>
-                        <Image
-                            style={styles.menuToolbar}
-                            source={require('../../../assets/House_picture.png')}
-                        />
-                    </TouchableHighlight>
-                    <TouchableHighlight onPress={() => navigation.navigate('Search')}>
-                        <Image
-                            style={styles.menuToolbar}
-                            source={require('../../../assets/Search_picture.png')}
-                        />
-                    </TouchableHighlight>
-                    <TouchableHighlight onPress={() => navigation.navigate('Settings')}>
-                        <Image
-                            style={styles.menuToolbar}
-                            source={require('../../../assets/Settings_picture.png')}
-                        />
-                    </TouchableHighlight>
-                </View>
-            
+
+            </KeyboardAwareScrollView>
+            <View style={styles.row}>
+                <TouchableHighlight onPress={() => navigation.navigate('MyProfile')}>
+                <Icon
+                        reverse
+                        name='ios-person'
+                        type='ionicon'
+                        color='#f9ad30'
+                    />
+                </TouchableHighlight>
+                <TouchableHighlight onPress={() => navigation.navigate('BCOverview')}>
+                <Icon
+                        reverse
+                        name='ios-book'
+                        type='ionicon'
+                        color='#f9ad30'
+                    />
+                </TouchableHighlight>
+                <TouchableHighlight onPress={() => navigation.navigate('StartPage')}>
+                    <Icon
+                        reverse
+                        name='ios-home'
+                        type='ionicon'
+                        color='#f9ad30'
+                    />
+                </TouchableHighlight>
+                <TouchableHighlight onPress={() => navigation.navigate('Search')}>
+                <Icon
+                        reverse
+                        name='ios-search'
+                        type='ionicon'
+                        color='#f9ad30'
+                    />
+                </TouchableHighlight>
+                <TouchableHighlight onPress={() => navigation.navigate('Settings')}>
+                <Icon
+                        reverse
+                        name='ios-settings'
+                        type='ionicon'
+                        color='#f9ad30'
+                    />
+                </TouchableHighlight>
+            </View>
+
         </View>
     );
 }
 
 export default MyProfile;
 
+/*<Image
+                            style={styles.menuToolbar}
+                            source={require('../../../assets/House_picture.png')}
+
+                            <Image
+                        style={styles.menuToolbar}
+                        source={require('../../../assets/BookClubs_picture.png')}
+                    />
+                        />
+                        
+                         <Image
+                        style={styles.menuToolbar}
+                        source={require('../../../assets/Settings_picture.png')}
+                    />
+                    <Image
+                        style={styles.menuToolbar}
+                        source={require('../../../assets/Search_picture.png')}
+                    />
+                    <Image
+                        style={styles.menuToolbar}
+                        source={require('../../../assets/Profile_picture.png')}
+                    />
+                        */
