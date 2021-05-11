ac1bd857fe21cdc4f9499ddff6776a73c862c2e1
Fredrika-B <fredrika.brink.7629@student.uu.se>
2021-05-11 16:01:05 +0200

    check if book exist in club

--- src/components/bcView/index.js
+++ src/components/bcView/index.js
@@ -1,15 +1,12 @@
 import React, { useEffect, useState, useRef } from 'react';
-import styles from '../styles';
 import { useDispatch, useSelector } from 'react-redux';
 import { View, Text, TouchableOpacity, TouchableHighlight, Image, ImageBackground } from 'react-native';
-import { createStructuredSelector } from 'reselect';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { ScrollView, TextInput } from 'react-native-gesture-handler';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { Icon } from 'react-native-elements'
 
-import { selectCartItems, selectReadingItems, selectClubItems } from '../../redux/Cart/cart.selectors';
-import { fetchBCStart, createCommentStart } from '../../redux/BookClub/bc.actions';
+import { createCommentStart } from '../../redux/BookClub/bc.actions';
+import styles from '../styles';
 
 const mapState = ({ user }) => ({
   currentUser: user.currentUser,
@@ -19,139 +16,99 @@ const mapState = ({ user }) => ({
 const mapStateBC = ({ bookclub }) => ({
   bc: bookclub.bc,
   bcCom: bookclub.comments
- // currentBC: bookclub.currentBC
-});
-
-const mapStateCart = createStructuredSelector({
-  cartItems: selectCartItems,
-  readingItems: selectReadingItems,
-  clubItems: selectClubItems
 });
 
 const BCView = ({ route, navigation }) => {
   const { currentUser } = useSelector(mapState);
-  const { updatedUser } = useSelector(mapState);
   const { bc } = useSelector(mapStateBC);
   const { bcCom } = useSelector(mapStateBC);
-  const { cartItems } = useSelector(mapStateCart);
-  const { readingItems } = useSelector(mapStateCart);
-  const { clubItems } = useSelector(mapStateCart);
   const [comment, setComment] = useState('');
   const dispatch = useDispatch();
- 
-  //const [clubID, setClubID] = useState('');
- 
   const { groupName } = route.params;
 
   useEffect(() => {
     mapClubToId();
-    
-    
 
   }, [bc, bcCom]
   );
 
   const getTime = () => {
     const timeStamp = Date.now();
-    const fixedTime = new Intl.DateTimeFormat('en-US', {
-      year: 'numeric', month: '2-digit', day: '2-digit',
-      hour: '2-digit', minute: '2-digit'
+    const fixedTime = new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit'
     }).format(timeStamp);
     return (fixedTime)
   }
 
   const mapClubToId = () => {
-
-   
     let chosenClub = bc.find(club => club.groupName === groupName);
-    
     if (chosenClub) {
-      
       const members = chosenClub.members;
       return (
         <ScrollView vertical={true} style={styles.rowBooks}>
           {members.map((member, index) => {
             return (
               <View key={member.email}>
-                <Text style={styles.middleTextOrange}><Text style={styles.capital}>- {member.displayName}</Text></Text>
+                <Text style={[styles.middleTextOrange, styles.capital]}>- {member.displayName}</Text>
               </View>
             )
           })}
         </ScrollView>)
     }
-    
   };
 
   const mapComment = () => {
-    if (bcCom.groupName != undefined){
+    const scrollViewRef = useRef();
+    if (bcCom.groupName != undefined) {
       let chosenClub = bcCom
-
       if (chosenClub) {
         const comments = chosenClub.comments;
-       
         return (
-          <ScrollView> 
+          <ScrollView
+            ref={scrollViewRef}
+            onContentSizeChange={() => scrollViewRef.current.scrollToEnd({ animated: true })}>
             {comments.map((comment, index) => {
-              return(     
+              return (
                 <View key={comment.comment}>
-                  
-                  <Text style={styles.middleTextPink}> {comment.user}:
+                  <Text style={styles.smallerGreyText}> {comment.time} </Text>
+                  <Text style={[styles.middleTextPink, styles.capital]}> {comment.user}:
                   <Text style={styles.smallMiddleText}> {comment.comment} </Text>
-                  <Text style={styles.smallerGreyText}> {comment.time} </Text> 
-                  </Text>
+                    </Text>
                 </View>
-  
-              )
-            })}
+              )})}
           </ScrollView>
-  
-        )
-      }
-
-    }
-    else{
-      let chosenClub = bc.find(club => club.groupName === groupName);  
-      if (chosenClub) {     
+        )}}
+    else {
+      let chosenClub = bc.find(club => club.groupName === groupName);
+      if (chosenClub) {
         const comments = chosenClub.comments;
-
         return (
-          <ScrollView> 
+          <ScrollView
+            ref={scrollViewRef}
+            onContentSizeChange={() => scrollViewRef.current.scrollToEnd({ animated: true })}>
             {comments.map((comment, index) => {
-              return(     
-                <View key={comment.comment}>                
-                  <Text style={styles.middleTextPink}> {comment.user}:
-                  <Text style={styles.smallMiddleText}> {comment.comment} </Text>
-                  <Text style={styles.smallerGreyText}> {comment.time} </Text> 
-                  </Text>
-                </View> 
-              )
-            })}
+              return (
+                <View key={comment.comment}>
+                  <Text style={styles.smallerGreyText}> {comment.time} </Text>
+                  <Text style={[styles.middleTextPink, styles.capital]}> {comment.user}:
+                  <Text style={styles.smallMiddleText}> {comment.comment} </Text></Text>
+                </View>
+              )})}
           </ScrollView>
-  
-        )
-      }
-    }
+        )}}
   };
-
-
 
   const handleCreateComment = () => {
     const timeStamp = getTime();
     setComment('');
-  
     let chosenClub = bc.find(club => club.groupName === groupName);
-    
-    console.log(chosenClub.documentID)
     if (chosenClub) {
       const clubID = chosenClub.documentID;
       dispatch(createCommentStart({
         comment,
         currentUser,
         clubID,
-        timeStamp}
-      ))
-      
-    }
+        timeStamp
+      }))}
   }
 
   const getBooks = () => {
@@ -161,32 +118,55 @@ const BCView = ({ route, navigation }) => {
       return (
         <ScrollView horizontal={true}
           vertical={true}
-          style={styles.rowBooks}>  
+          style={styles.rowBooks}>
           {books.map((bookObject, index) => {
             const book = bookObject.book;
             if (bookObject.read === false) {
-            return (
-              <View key={book.id}>
-                <View style={styles.square}>
-                <Text style={styles.smallThinTextOrange}> {book.title} </Text>
+              return (
+                <View key={book.id}>
+                  <View style={styles.square}>
+                    <Text style={styles.smallThinTextOrange}> {book.title} </Text>
+                  </View>
+                  <Text style={styles.smallerText}> {book.author} </Text>
+                  <Image
+                    style={styles.bookImageSmall}
+                    source={{ uri: book.picture }} />
+                  <TouchableOpacity
+                    style={styles.smallButton}
+                    onPress={() => navigation.navigate('Rating', book)}>
+                    <Text> Finished reading </Text>
+                  </TouchableOpacity>
                 </View>
-                <Text style={styles.smallerText}> {book.author} </Text>
-                <Image
-                  style={styles.bookImageSmall}
-                  source={{ uri: book.picture }} />
-                 <TouchableOpacity
-                        style={styles.smallButton}
-                        onPress={() => navigation.navigate('Rating', book)}>
-                        <Text> Finished reading </Text>
-                 </TouchableOpacity>  
-              </View>
-            )
-            }
-          })}
+              )}})}
         </ScrollView>)
     }
   }
-  const bookUrlTest = { uri: 'https://images-na.ssl-images-amazon.com/images/I/41gznIDw41L._SX326_BO1,204,203,200_.jpg' }
+
+  const getHistoryBooks = () => {
+    let chosenClub = bc.find(club => club.groupName === groupName);
+    if (chosenClub) {
+      const books = chosenClub.bcbooks;
+      return (
+        <ScrollView horizontal={true}
+          vertical={true}
+          style={styles.rowBooks}>
+          {books.map((bookObject, index) => {
+            const book = bookObject.book;
+            if (bookObject.read === true) {
+              return (
+                <View key={book.id}>
+                  <TouchableOpacity
+                    onPress={() => navigation.navigate('Rating', book)}>
+                    <Image
+                      style={styles.bookImageSmall}
+                      source={{ uri: book.picture }} />
+                  </TouchableOpacity>
+                </View>
+              )}})}
+        </ScrollView>)
+    }
+  }
+
   return (
     <View style={styles.container}>
       <KeyboardAwareScrollView style={{ width: '100%', height: '90%' }}>
@@ -204,68 +184,34 @@ const BCView = ({ route, navigation }) => {
           <View style={styles.whiteSquare}>
             <Text>{mapClubToId()}</Text>
           </View>
+
           <Text style={styles.textLeft}>History </Text>
           <Text style={styles.smallBlackText}>Books you've read together: </Text>
-          <SafeAreaView>
-            <ScrollView horizontal={true}
-              vertical={true}
-              style={styles.rowBooks}>
-              <Image //här får vi kanske loopa igenom de böcker som klubben har läst?
-                style={styles.bookImageSmall}
-                source={bookUrlTest} />
-              <Image
-                style={styles.bookImageSmall}
-                source={require('../../../assets/sherlock.jpg')} />
-              <Image
-                style={styles.bookImageSmall}
-                source={bookUrlTest} />
-              <Image
-                style={styles.bookImageSmall}
-                source={require('../../../assets/sherlock.jpg')} />
-              <Image
-                style={styles.bookImageSmall}
-                source={bookUrlTest} />
-              <Image
-                style={styles.bookImageSmall}
-                source={bookUrlTest} />
-              <Image
-                style={styles.bookImageSmall}
-                source={bookUrlTest} />
-              <Image
-                style={styles.bookImageSmall}
-                source={require('../../../assets/sherlock.jpg')} />
-              <Image
-                style={styles.bookImageSmall}
-                source={require('../../../assets/sherlock.jpg')} />
-              <Image
-                style={styles.bookImageSmall}
-                source={bookUrlTest} />
+          {getHistoryBooks()}
 
-            </ScrollView>
-            </SafeAreaView>
-            <Text style={styles.textLeft}> Discussion </Text>
-            <View style={styles.whiteMediumSquare}>
+          <Text style={styles.textLeft}> Discussion </Text>
+          <View style={styles.whiteMediumSquare}>
             <Text>{mapComment()}</Text>
-            </View>
-            <Text style={styles.smallText}>Make a comment</Text>
-            <TextInput
-              multiline
-              style={styles.inputComment}
-              placeholder="Make a comment"
-              placeholderTextColor="#aaaaaa"
-              onChangeText={(text) => setComment(text)}
-              value={comment}
-              autoCapitalize="sentences"
-              />
+          </View>
+          <Text style={styles.textLeft}>Make a comment</Text>
+          <TextInput
+            multiline
+            style={styles.inputComment}
+            placeholder="Make a comment"
+            placeholderTextColor="#aaaaaa"
+            onChangeText={(text) => setComment(text)}
+            value={comment}
+            autoCapitalize="sentences"
+          />
 
-              <TouchableOpacity 
-              style={styles.smallButtonComment}
-              onPress={() => handleCreateComment()}
-              >
-              <Text>Post comment</Text>
-              </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.smallButtonComment}
+            onPress={() => handleCreateComment()}
+          >
+            <Text>Post comment</Text>
+          </TouchableOpacity>
 
-        
+
           <Text style={styles.textLeft}> Questions </Text>
           <View style={styles.whiteSquare}>
             <Text style={styles.smallMiddleText}>- What is the significance of the title? Did you find it meaningful, why or why not?</Text>
@@ -338,17 +284,3 @@ const BCView = ({ route, navigation }) => {
 export default BCView;
 
 
-/* old get book club books
- {clubItems.map((clubItem, index) => {
-              const bookurl = { uri: clubItem.picture }
-              return (
-                <View key={clubItem.title} style={styles.center}>
-                  <Text style={styles.smallTextOrange}> {clubItem.title} </Text>
-                  <Text style={styles.smallerText}> {clubItem.author} </Text>
-                  <Image
-                    style={styles.bookImageMedium}
-                    source={bookurl} />
-                </View>
-              )
-            })}
-            */
